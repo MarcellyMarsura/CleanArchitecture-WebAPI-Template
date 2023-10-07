5f9a5e9692f0937c5a44227cc4bb28f84ae5a36d
Marcelly Marsura <marcellymarsura@gmail.com>
2023-10-07 00:47:04 -0300

    feat: Adicionar ExemploRepository no projeto Infrastructure.Data

--- WebAPI.Domain/Interfaces/IRepository.cs
+++ WebAPI.Domain/Interfaces/IRepository.cs
@@ -6,18 +6,10 @@ namespace WebAPI.Domain.Interfaces
     public interface IRepository<T> where T : Entity
     {
         int Create(T entity);
-        void Update(T entity);
-        void Delete(T entity);
         T FindById(int id);
-        T[] FindAll(int page = 1, int quantity = 25, string orderBy = "Id", string order = "asc");
-        T[] FindBy(Expression<Func<T, bool>> predicate);
-        int Count();
-        int Create(IEnumerable<T> entities);
-        int DeleteBy(Expression<Func<T, bool>> predicate);
-        int Update(IEnumerable<T> entities);
-        bool CreateOrUpdate(T entity);
-        int CreateOrUpdate(IEnumerable<T> entities);
-        bool DropCollection(string collectionName);
-        void CreateTable();
+        List<T> FindAll();
+        void Update(T entity);
+        void Delete(int id);
+        
     }
 }
